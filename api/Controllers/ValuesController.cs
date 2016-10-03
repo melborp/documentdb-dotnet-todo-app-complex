a7e37e64ee1c155804a0ea8ab4429d66439595a6
Taavi Koosaar <taavik@melborp.net>
2016-10-03 23:50:04 +0200

    api with sql

---
+++ api/Controllers/ValuesController.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Swashbuckle.Swagger.Annotations;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace api.Controllers
+{
+    public class ValuesController : ApiController
+    {
+        string connectionString;
+        public ValuesController()
+        {
+            connectionString = ConfigurationManager.AppSettings["ConnectionString"];
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand("select count(1) from SalesLT.Product", connection);
+                connection.Open();
+
+                var count = command.ExecuteScalar();
+            }
+        }
+
+        // GET api/values
+        [SwaggerOperation("GetAll")]
+        public IEnumerable<string> Get()
+        {
+            List<string> names = new List<string>();
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand("select top 100 Name from SalesLT.Product", connection);
+                connection.Open();
+
+                var reader = command.ExecuteReader(System.Data.CommandBehavior.CloseConnection);
+                while (reader.Read())
+                {
+                    names.Add(reader[0]?.ToString());
+                }
+                reader.Close();
+            }
+            return names;
+        }
+
+        // GET api/values/5
+        [SwaggerOperation("GetById")]
+        [SwaggerResponse(HttpStatusCode.OK)]
+        [SwaggerResponse(HttpStatusCode.NotFound)]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [SwaggerOperation("Create")]
+        [SwaggerResponse(HttpStatusCode.Created)]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        [SwaggerOperation("Update")]
+        [SwaggerResponse(HttpStatusCode.OK)]
+        [SwaggerResponse(HttpStatusCode.NotFound)]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [SwaggerOperation("Delete")]
+        [SwaggerResponse(HttpStatusCode.OK)]
+        [SwaggerResponse(HttpStatusCode.NotFound)]
+        public void Delete(int id)
+        {
+        }
+    }
+}
